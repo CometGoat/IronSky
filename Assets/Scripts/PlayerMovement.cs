1f2a053b3e7491e82430e397ff83e1294881b173
Phill Serrazina <fserrazina99@gmail.com>
2019-12-17 12:52:36 +0000

    Implemented Basic Movement Logic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,35 +4,68 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // VARIABLES
 
     PlayerAnimation playerAnim;
+    Vector2 movementDirection;
+    [SerializeField] float speed = 5f;
+
+    bool turningLeft;
+    bool turningRight;
+
+    Rigidbody2D rb;
+
+    // EXECUTION FUNCTIONS
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         playerAnim = GetComponent<PlayerAnimation>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
+        GetInput();
+
+        /*
+        if (Input.GetKeyDown(KeyCode.A)) playerAnim.TurnLeft();
+        if (Input.GetKeyUp(KeyCode.A)) playerAnim.ReturnLeft();
+
+        if(Input.GetKeyDown(KeyCode.D)) playerAnim.TurnRight();
+        if(Input.GetKeyUp(KeyCode.D)) playerAnim.ReturnRight();
+        */
+    }
+
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + (movementDirection * speed) * Time.fixedDeltaTime);
+    }
+
+    // METHODS
+
+    void GetInput() {
+        movementDirection.x = Input.GetAxisRaw("Horizontal");
+        movementDirection.y = Input.GetAxisRaw("Vertical");
+
+        if (movementDirection.x < 0 && !turningLeft) {
             playerAnim.TurnLeft();
+            turningLeft = true;
         }
-
-        if (Input.GetKeyUp(KeyCode.A))
-        {
+        if (movementDirection.x == 0 && turningLeft) {
             playerAnim.ReturnLeft();
+            turningLeft = false;
         }
 
-        if(Input.GetKeyDown(KeyCode.D))
-        {
+        if (movementDirection.x > 0 && !turningRight) {
             playerAnim.TurnRight();
+            turningRight = true;
         }
-
-        if(Input.GetKeyUp(KeyCode.D))
-        {
-            playerAnim.ReturnRight();
+        if (movementDirection.x == 0 && turningRight) {
+            playerAnim.ReturnRight(); 
+            turningRight = false;
         }
+    }
 
+    void Movement() {
+        
     }
 }
